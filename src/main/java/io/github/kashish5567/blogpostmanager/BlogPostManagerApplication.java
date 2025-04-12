b8969e460fcf3c9ebd7bcc619d153f772bbb03ee
Kashish5567 <Kashishgoyal652@gmail.com>
2025-04-12 22:35:34 +0530

    Initial commit

---
+++ src/main/java/io/github/kashish5567/blogpostmanager/BlogPostManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.kashish5567.blogpostmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPostManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPostManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/kashish5567/blogpostmanager/BlogPostManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kashish5567.blogpostmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPostManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
